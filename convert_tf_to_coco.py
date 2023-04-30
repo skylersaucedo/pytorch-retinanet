65e3570a83e3f25dd4fba92d35efef5efd9f24c5
skylersaucedo <skyler.saucedo@gmail.com>
2023-04-30 10:00:46 -0500

    first commit

---
+++ convert_tf_to_coco.py
@@ -0,0 +1,98 @@
+"""
+Use this to convert csv -> df -> coco
+"""
+
+import numpy as np
+import json
+import pandas as pd
+import os
+
+# use this method to convert csv to json
+
+def convert_to_coco(data, save_json_path):
+    #path = 'tensorflow2csv.csv'
+    #data = pd.read_csv(path)
+
+    images = []
+    categories = []
+    annotations = []
+
+    category = {}
+    category["supercategory"] = 'none'
+    category["id"] = 0
+    category["name"] = 'None'
+    categories.append(category)
+
+    data['fileid'] = data['filename'].astype('category').cat.codes
+    data['categoryid']= pd.Categorical(data['class'],ordered= True).codes
+    data['categoryid'] = data['categoryid']+1
+    data['annid'] = data.index
+
+    def image(row):
+        image = {}
+        image["height"] = row.height
+        image["width"] = row.width
+        image["id"] = row.fileid
+        image["file_name"] = row.filename
+        return image
+
+    def category(row):
+        category = {}
+        category["supercategory"] = 'None'
+        category["id"] = row.categoryid
+        category["name"] = row[2]
+        return category
+
+    def annotation(row):
+        annotation = {}
+        area = (row.xmax -row.xmin)*(row.ymax - row.ymin)
+        annotation["segmentation"] = []
+        annotation["iscrowd"] = 0
+        annotation["area"] = area
+        annotation["image_id"] = row.fileid
+
+        annotation["bbox"] = [row.xmin, row.ymin, row.xmax -row.xmin,row.ymax-row.ymin ]
+
+        annotation["category_id"] = row.categoryid
+        annotation["id"] = row.annid
+        return annotation
+
+    for row in data.itertuples():
+        annotations.append(annotation(row))
+
+    imagedf = data.drop_duplicates(subset=['fileid']).sort_values(by='fileid')
+    for row in imagedf.itertuples():
+        images.append(image(row))
+
+    catdf = data.drop_duplicates(subset=['categoryid']).sort_values(by='categoryid')
+    for row in catdf.itertuples():
+        categories.append(category(row))
+
+    data_coco = {}
+    data_coco["images"] = images
+    data_coco["categories"] = categories
+    data_coco["annotations"] = annotations
+
+
+    json.dump(data_coco, open(save_json_path, "w"), indent=4)
+
+def main():
+    #dddd
+    root = os.getcwd()
+    print(root)
+
+    train_csv_path = r'C:\Users\endle\Desktop\pytorch-retinanet\train_labels  - org.csv'
+    test_csv_path = r'C:\Users\endle\Desktop\pytorch-retinanet\test_labels - org.csv'
+
+    df_train = pd.read_csv(train_csv_path)
+    df_test = pd.read_csv(test_csv_path)
+
+    save_json_path_train = r'C:\Users\endle\Desktop\pytorch-retinanet\train_new.json'
+    save_json_path_val = r'C:\Users\endle\Desktop\pytorch-retinanet\val_new.json'
+
+    convert_to_coco(df_train, save_json_path_train)
+    convert_to_coco(df_test, save_json_path_val)
+
+
+if __name__ == "__main__":
+    main()
